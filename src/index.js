4ff2d8d497ad5f2bb3ad4db35b7158b8052844d1
anastasia <novakanastasya@yandex.ru>
2018-03-04 21:12:55 +0300

    Implementation of longestConsecutiveLength

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const numsSet = new Set(array);
+  let longStreak = 0;
+
+  for (let i of numsSet) {
+    if (numsSet.has(i - 1) === false) {
+      let currentValue = i;
+      let currentStreak = 1;
+
+      while (numsSet.has(currentValue + 1)) {
+        currentValue += 1;
+        currentStreak += 1;
+      }
+
+      longStreak = Math.max(currentStreak, longStreak);
+    }
+  }
+
+  return longStreak;
 }
